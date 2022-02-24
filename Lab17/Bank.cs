f1f03e2e6dea28824f55eb89f7374b6987a7f991
Михаил <stulstoldevyat@gmail.com>
2022-02-24 22:52:58 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab17
+{
+    class Bank<T>
+    {
+        public T AccountName { get; set; }
+        public double Price { get; set; }
+        public string NamePerson { get; set; }
+        public Bank(T account, double price,string name)
+        {
+            this.AccountName = account;
+            this.Price = price;
+            this.NamePerson = name;
+        }
+        public string Info()
+        {
+            return $"{AccountName} {Price} {NamePerson}";
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета");
+            int accountInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс счета");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string name = Console.ReadLine();
+            Bank<int> bank = new Bank<int>(accountInt, price, name);
+            Console.WriteLine(bank.Info());
+
+            Console.WriteLine("Введите строковый номер счета");
+            string accountString = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета");
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            name = Console.ReadLine();
+            Bank<string> bank1 = new Bank<string>(accountString, price, name);
+            Console.WriteLine(bank1.Info());
+            
+            Console.ReadKey();
+        }
+    }
+}
